3162fc58dd64e1b22f554c1be2f3d85b154d7673
Ryan McIntyre <ryankmcintyre@gmail.com>
2016-10-27 13:03:50 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var init = require('./config/init')(),
 	mongoose = require('mongoose'),
 	chalk = require('chalk');
 
+
 /**
  * Main application entry file.
  * Please note that the order of loading is important.
